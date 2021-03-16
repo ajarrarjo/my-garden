5f98700ad5f5a0808e36fca68f4f63219d52d4f4
ajarrarjo <ajarrarjo@yahoo.com>
2021-03-16 12:33:27 +0200

    failed

---
+++ css/Vanilla.js
@@ -0,0 +1,13 @@
+//keep info on reload
+
+// Handle adding the flowers to the HTML using the DOM and local storage.
+// Handle getting the flowers from the local storage and rendering them.
+// Handle removing the selected flowers from the list.
+
+
+
+ 
+const trbody= document.createElement('tr');
+
+const tdtag1 = document.createElement("th");
+
