ae0d14f3309bbc62a13cf728d06dcbc2a11a1493
ajarrarjo <ajarrarjo@yahoo.com>
2021-03-16 12:48:17 +0200

    update

--- css/Vanilla.js
+++ css/Vanilla.js
@@ -1,13 +1,131 @@
-//keep info on reload
+const Student=function (id,email,phone,tuition)
+{
+    this.id=id;
+    this.name=email.split('@')[0];  
+    this.email=email;
+    this.phone=phone;
+    this.tuition=tuition;
+    this.age=this.randomAge(18,24);
+    Student.all.push(this);  
 
-// Handle adding the flowers to the HTML using the DOM and local storage.
-// Handle getting the flowers from the local storage and rendering them.
-// Handle removing the selected flowers from the list.
+};
 
 
+
+Student.all=[]
+
+
+//prototype is fixed , Student same with name of constructor ,addStudent any name you want
+Student.prototype.addStudent=function()
+{    
+     localStorage.setItem('data',JSON.stringify(Student.all));
+};
+
+Student.prototype.randomAge=function(max,min)
+{
+    return Math.floor( Math.random() * (max - min + 1) + min)
+};
+
+
+
+let handelSubmit=function(event)
+{
+   
+    event.preventDefault();
+
+    let newEntry=new Student(Student.all.length+1,document.getElementById('email').value,
+    document.getElementById('phone').value, document.getElementById('tuition').value);
+
+
+    newEntry.addStudent();
+    
+    renderFunction();
+};
+
+
+let renderFunction=function()
+{
+    if(localStorage.data)
+    {
+     
+        body.innerHTML='';
+        for (let index = 0; index < JSON.parse(localStorage.data).length; index++) {
+            
+            let trTag=document.createElement('tr');
+
+           
+            let tdTagID=document.createElement('td');
+            tdTagID.textContent=Student.all[index].id;
+trTag.appendChild(tdTagID);
+
+
+
+let tdTagIDname=document.createElement('td');
+tdTagIDname.textContent=Student.all[index].name;
+trTag.appendChild(tdTagIDname);
+
+
+
+            let tdTagemail=document.createElement('td');
+            tdTagemail.textContent=Student.all[index].email;
+            trTag.appendChild(tdTagemail);
+
+
+       
+
+            let tdTagphone=document.createElement('td');
+            tdTagphone.textContent=Student.all[index].phone;
+            trTag.appendChild(tdTagphone);
+
+            let tdTagage=document.createElement('td');
+            tdTagage.textContent=Student.all[index].age;
+            trTag.appendChild(tdTagage);
+
+
+            let tdTagIDtuition=document.createElement('td');
+            tdTagIDtuition.textContent=Student.all[index].tuition;
+            trTag.appendChild(tdTagIDtuition);
+
+
+
+            body.appendChild(trTag);
+
+   
+
+
+        }
 
  
-const trbody= document.createElement('tr');
 
-const tdtag1 = document.createElement("th");
-tdtag1.setAttribute('id', 'delete');
+    }
+}
+
+let addLocalStorageToArray=function () {
+    if(localStorage.data)
+    {
+        for (let index = 0; index < JSON.parse(localStorage.data).length; index++) {
+            console.log(JSON.parse(localStorage.data)[0])
+          Student.all.push(JSON.parse(localStorage.data)[index]);
+
+     
+            
+        }
+    }
+}
+
+
+
+
+let body=document.getElementById('body');
+
+
+let form=document.getElementById('form');//DOM
+    form.addEventListener('submit',handelSubmit);
+
+    addLocalStorageToArray();
+    renderFunction();
+
+
+
+  
+
